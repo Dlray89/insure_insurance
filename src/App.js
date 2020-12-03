e205b7f302aed5d06f97b30b9614a2ac15dbbe6f
Dlray89 <dlrayjr89@gmail.com>
2020-12-03 11:57:09 -0500

    added body to landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Nav-folder/Nav'
 import Header from './Components/header-folder/header'
+import Body from './Components/Body Content/body'
 import './App.css';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar  />
       <Header  />
+      <Body />
     </div>
   );
 }
