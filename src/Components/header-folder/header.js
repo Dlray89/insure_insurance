011ed9bf24641381433db4c5ac177aabcfe4e513
Dlray89 <dlrayjr89@gmail.com>
2020-12-02 22:06:57 -0500

    header coming along nicely after figuring out zIndex

--- src/Components/header-folder/header.js
+++ src/Components/header-folder/header.js
@@ -2,36 +2,37 @@ import React from 'react'
 import { makeStyles, Button } from '@material-ui/core'
 import Header_pic from '../../images/header_pic.jpg'
 import rightPattern from '../../images/bg-pattern-intro-right-desktop.svg'
+import LeftPattern from '../../images/leftPattern.svg'
 
 const useStyles = makeStyles((theme) => ({
     mainContainer: {
-        border: 'solid 2px blue',
+        border: 'solid 2px yellow',
         background: 'hsl(256, 26%, 20%)',
         color: 'white',
         display:'flex',
         boxSizing:'border-box',
-        justifyContent:'space-evenly'
+        width:'100%',justifyContent:'center'
+        
 
     },
     firstBox:{
         border: 'solid 2px blue',
-        width:'100%',
+        width:'60%',
         boxSizing:'border-box',
-        marginLeft:'5%',
-        marginRight:'5%'
+       
     },
     fbText1:{
         border: 'solid 2px red',
         width:'70%',
-        margin:'0 auto',
+        margin:'5% auto',
         textAlign:'start',
         boxSizing:'border-box',
-        marginTop:'15%'
+        
     },
     fbText2:{
         border: 'solid 2px red',
         width:'70%',
-        margin:'0 auto',
+        margin:'2% auto',
         fontSize:'50px',
         textAlign:'start',
         boxSizing:'border-box'
@@ -39,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
     fbText3:{
         border: 'solid 2px red',
         width:'70%',
-        margin:'0 auto',
+        margin:'2% auto',
         boxSizing:'border-box',
         textAlign:'start',
     },
@@ -65,19 +66,30 @@ const useStyles = makeStyles((theme) => ({
         border: 'solid 2px green',
         color:'white',
         boxSizing:'border-box',
-        marginLeft:'20%',
-        width:'70%'
+        width:'30%',
+        height:'75vh',
+        backgroundImage:'url('+ Header_pic+')',
+        backgroundRepeat:'no-repeat',
+        backgroundSize:'100%',
+        position:'absolute',
+        left:'54%',
+        top:'24%'
     },
-    sbImg:{
-        border: 'solid 2px blue',
-        width:'25%',
-        position:'relative',
-        top:'40%'
-       
-    },
+   
     rightpattern:{
-        width:'20%',
+        width:'60%',
         
+    },
+    rightPatternBox:{
+        width:'40%', 
+        textAlign:'right',
+        zIndex:1
+    },
+    leftPatternBox:{
+        border:'solid 2px blue',
+        position:'absolute',
+        left:'0%',
+        top:'60%'
     }
 }))
 
@@ -85,14 +97,16 @@ const Header = () => {
     const classes = useStyles()
     return (
         <div className={classes.mainContainer}>
-
+            <div className={classes.leftPatternBox}>
+                <img src={LeftPattern} />
+            </div>
             <div className={classes.firstBox}>
                 <div className={classes.fbText1}>
                     ______________________
             </div>
 
                 <div className={classes.fbText2}>
-                    Humanizing Your Insurance.
+                    Humanizing <br />Your Insurance.
             </div>
 
                 <div className={classes.fbText3}>
@@ -107,15 +121,18 @@ const Header = () => {
 
            
                 
+               <div className={classes.secondBox}>
                
-                <img className={classes.sbImg} src={Header_pic} alt='man holding child hands' />
+               </div>
                 
                
 
            
-
-         
+            <div className={classes.rightPatternBox}>
             <img className={classes.rightpattern} src={rightPattern} alt='design lines' />
+            
+            </div>
+            
             
 
         </div>
